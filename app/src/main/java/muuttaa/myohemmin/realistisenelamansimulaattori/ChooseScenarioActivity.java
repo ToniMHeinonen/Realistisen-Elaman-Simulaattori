6f1eeddc87e38366fa54d3380159ae114380bb42
Toni <toni.heinonen@hotmail.fi>
2020-02-19 10:14:29 +0200

    Initial commit

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ChooseScenarioActivity.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseScenarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
