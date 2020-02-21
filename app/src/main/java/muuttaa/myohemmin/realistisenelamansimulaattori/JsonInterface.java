55b79d5a3c30ccb31818ddfe698be3613963888c
OAhonen <olli.p.ahonen@tuni.fi>
2020-02-21 06:35:04 +0200

    Add methods to JsonInterface.

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/JsonInterface.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/JsonInterface.java
@@ -1,4 +1,13 @@
 package muuttaa.myohemmin.realistisenelamansimulaattori;
 
+import java.util.List;
+
 public interface JsonInterface {
+    public List getScenarioList();
+    public List getAnswersList();
+    public String getPictureName();
+    public String getJsonName();
+    public void setFirstSceneFromScenario();
+    public void nextScene();
+    public void setCurrentScenario();
 }
