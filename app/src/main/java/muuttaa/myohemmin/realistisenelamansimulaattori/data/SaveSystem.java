e77c61445030802deb3284a6718eaf58d0caf35e
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-02-25 16:07:13 +0200

    add: savedata scenaries search

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/data/SaveSystem.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/data/SaveSystem.java
@@ -3,19 +3,26 @@ package muuttaa.myohemmin.realistisenelamansimulaattori.data;
 import android.content.Context;
 import android.util.Log;
 
-import java.io.FileInputStream;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
 import muuttaa.myohemmin.realistisenelamansimulaattori.JsonInterface;
+import muuttaa.myohemmin.realistisenelamansimulaattori.R;
 
 public class SaveSystem implements JsonInterface {
-    private String FILENAME = "saveData.json";
+    private String FILENAME = "savedata.json";
     private Context context;
     private boolean debuggi = true;
+    private List<String> list;
 
     /**
      * This constructor get Context information
@@ -25,13 +32,26 @@ public class SaveSystem implements JsonInterface {
      */
     public SaveSystem(Context con){
         this.context = con;
+        this.list = new LinkedList<>();
     }
     @Override
     public List getScenarioList() {
-        List<String> list = new LinkedList<>();
+        list.clear();
         String data = getStringFromFile();
-
-        return null;
+        try {
+            JSONObject base = new JSONObject(data);
+            JSONArray help = base.getJSONArray("scenarios");
+            for(int index = 0; index < help.length(); index++){
+                String adding = help.getString(index);
+                list.add(adding);
+            }
+        }catch (JSONException e){
+            if(debuggi){
+                Log.e("JSON/SaveSystem", "Virhe muodostaessa json objektia");
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 
     @Override
@@ -63,32 +83,15 @@ public class SaveSystem implements JsonInterface {
     public void setCurrentScenario(String scenario) {
 
     }
-    private void writeStringToFile(String in){
-        try {
-            FileOutputStream fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
-            fos.write(in.getBytes());
-            fos.close();
-        } catch (FileNotFoundException e){
-            if(debuggi){
-                Log.e("FILE/SaveSystem/write", "Tiedostoa ei löytynyt");
-                e.printStackTrace();
-            }
-        } catch (IOException e){
-            if(debuggi){
-                Log.e("FILE/SaveSystem/write", "kirjoituksessa ongelmaa");
-                e.printStackTrace();
-            }
-        }
-    }
     private String getStringFromFile(){
         String out = "";
         try {
-            FileInputStream fis = context.openFileInput(FILENAME);
-            int byt;
-            while ((byt = fis.read()) != -1) {
-                out += (char) byt;
+            InputStream test = context.getResources().openRawResource(R.raw.savedata);
+            BufferedReader br = new BufferedReader(new InputStreamReader(test));
+            String nextLine;
+            while ((nextLine = br.readLine()) != null) {
+                out += nextLine;
             }
-            fis.close();
         } catch (FileNotFoundException e){
             if(debuggi){
                 Log.e("FILE/SaveSystem/get", "Tiedostoa ei löytynyt");
