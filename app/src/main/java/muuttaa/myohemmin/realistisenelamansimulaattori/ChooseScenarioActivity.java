3826e943b2a23bac20123e0894a64180d9cbecc3
Toni <toni.heinonen@hotmail.fi>
2020-02-28 09:13:43 +0200

    ChooseScenarioActivity: Add list logic

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ChooseScenarioActivity.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ChooseScenarioActivity.java
@@ -3,6 +3,13 @@ package muuttaa.myohemmin.realistisenelamansimulaattori;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class ChooseScenarioActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class ChooseScenarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView list = findViewById(R.id.list);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String clickedItem=(String) list.getItemAtPosition(position);
+                Toast.makeText(ChooseScenarioActivity.this,clickedItem,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
