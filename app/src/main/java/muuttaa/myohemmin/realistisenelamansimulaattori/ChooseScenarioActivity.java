baed97645ef03a1f8fbc8385b5b90a4c7b735040
OAhonen <olli.p.ahonen@tuni.fi>
2020-02-28 09:20:26 +0200

    Add temporarily button to ChooseScenarioActivity.

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ChooseScenarioActivity.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ChooseScenarioActivity.java
@@ -2,7 +2,10 @@ package muuttaa.myohemmin.realistisenelamansimulaattori;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseScenarioActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class ChooseScenarioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    For testing ScenarioActivity-class.
+     */
+    public void clicked(View v) {
+        Intent intent = new Intent(this, ScenarioActivity.class);
+        startActivity(intent);
     }
 }
