00e209a13ba9f2d98bbf6c5bda62fb2ddb22f021
OAhonen <olli.p.ahonen@tuni.fi>
2020-02-28 09:09:16 +0200

    Add ScenarioActivity.

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ScenarioActivity.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScenarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
