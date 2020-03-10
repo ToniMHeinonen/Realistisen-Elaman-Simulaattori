a88a65ccd905643a920662142e2f48733efa4aeb
Toni <toni.heinonen@hotmail.fi>
2020-03-10 10:08:37 +0200

    Add SettingsActivity and activity_settings.xml.

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/SettingActivity.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
