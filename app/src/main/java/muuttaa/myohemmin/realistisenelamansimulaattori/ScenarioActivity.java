f02396dad9ce07e85a273f0811dff4dd75bd8ede
OAhonen <olli.p.ahonen@tuni.fi>
2020-03-14 09:42:35 +0200

    Add: ScenarioActivity sends user's answers to GameOverActivity.

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ScenarioActivity.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/ScenarioActivity.java
@@ -1,13 +1,16 @@
 package muuttaa.myohemmin.realistisenelamansimulaattori;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,11 +21,16 @@ public class ScenarioActivity extends AppCompatActivity {
     private SaveSystem saveSystem;
     private TextView questionTextView;
     private List<String> colors;
+    private ArrayList<Integer> userAnswers;
+    private final int CORRECT = 100;
+    private final int WRONG = 0;
+    private final int SEMICORRECT = 50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario);
+        userAnswers = new ArrayList<>();
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             scenario = extras.getString("scenario");
@@ -44,46 +52,62 @@ public class ScenarioActivity extends AppCompatActivity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(final AdapterView<?> parent, final View view, final int position, long id) {
-                // Set background-color for button
+                // Set background-color for button depending user's answer.
                 view.setBackgroundResource(getResources()
                         .getIdentifier(String.valueOf(colors.get(position)),
                                 "drawable", getApplicationContext().
                                         getPackageName()));
+
+                // Add user's answer to list and set arrayadapter non-clickable.
+                addUserAnswerToList(colors.get(position));
                 list.setEnabled(false);
+
                 // Pause 1 second, then do the run-method.
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        // Set default background-color for button
+                        // Set default background-color for button.
                         view.setBackgroundResource(R.drawable.button_default);
                         String clickedItem = (String) list.getItemAtPosition(position);
-                        refresh(clickedItem);
-                        arrayAdapter.clear();
-                        arrayAdapter.addAll(saveSystem.getAnswersList());
-                        arrayAdapter.notifyDataSetChanged();
-                        list.setEnabled(true);
+                        saveSystem.nextScene(clickedItem);
+
+                        // If the question was last from the scenario, go to GameOverActivity
+                        // and send user's answers. Else update the questions in arrayadapter.
+                        if (saveSystem.endOfScenario()) {
+                            Intent intent = new Intent(getApplicationContext(), GameOverActivity.class);
+                            intent.putExtra("userAnswers", userAnswers);
+                            startActivity(intent);
+                        } else {
+                            colors = saveSystem.getColorsInString();
+                            questionTextView.setText(saveSystem.getQuestionFromScenario());
+                            arrayAdapter.clear();
+                            arrayAdapter.addAll(saveSystem.getAnswersList());
+                            arrayAdapter.notifyDataSetChanged();
+                            list.setEnabled(true);
+                        }
                     }
                 }, 1000);
             }
         });
     }
 
-    private void refresh(String clickedItem) {
-        saveSystem.nextScene(clickedItem);
-        colors = saveSystem.getColorsInString();
-        questionTextView.setText(saveSystem.getQuestionFromScenario());
+    /**
+     * Add 100% if player answered correctly, 50% if answer was "acceptable" and
+     * 0% if answer was wrong.
+     * @param color color of the answer
+     */
+    private void addUserAnswerToList(String color) {
+        switch (color) {
+            case "green":
+                userAnswers.add(CORRECT);
+                break;
+            case "yellow":
+                userAnswers.add(SEMICORRECT);
+                break;
+            case "red":
+                userAnswers.add(WRONG);
+                break;
+        }
     }
-
-        /*
-        Testing
-
-        SaveSystem save = new SaveSystem(this);
-        save.setCurrentScenario("BMW");
-        Log.d("ScenarioActivity", String.valueOf(save.getScenarioList()));  // [Ford, BMW, Fiat]
-        Log.d("ScenarioActivity", save.getJsonName());                      // bmw.json
-        Log.d("ScenarioActivity", save.getQuestionFromScenario());          // Mikä on kaupunki?
-        Log.d("ScenarioActivity", String.valueOf(save.getAnswersList()));   // [eka vastaus, ...]
-        Log.d("ScenarioActivity", save.getPictureName());                   // kuva1
-         */
 }
