ff74f87fe4c3d3db7e6835221b25078b2ff37014
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-22 16:57:03 +0200

    add: create basic activity

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/CreateScenario.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateScenario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_scenario);
+    }
+}
