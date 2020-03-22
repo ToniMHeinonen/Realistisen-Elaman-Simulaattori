50bfbe0f46b6489fbe81762a0fdae2265e1b6fd7
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-22 17:01:55 +0200

    add: new activity for scene creation

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/CreateScene.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateScene extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_scene);
+    }
+}
