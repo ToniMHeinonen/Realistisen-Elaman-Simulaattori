6b5f28e5c58ca22e56dc6bbf1fb697531ee1a100
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-24 14:35:44 +0200

    change: Spinner values for answer creation

--- app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/AnswerCreation.java
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/AnswerCreation.java
@@ -1,14 +1,20 @@
 package muuttaa.myohemmin.realistisenelamansimulaattori;
 
 import androidx.appcompat.app.AppCompatActivity;
+import muuttaa.myohemmin.realistisenelamansimulaattori.data.GeneralKeyAndValue;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AnswerCreation extends AppCompatActivity {
+    private Spinner varit;
+    private EditText menee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +34,20 @@ public class AnswerCreation extends AppCompatActivity {
         params.y = -20;
 
         getWindow().setAttributes(params);
+
+        this.varit = (Spinner) findViewById(R.id.vastausVari);
+        this.menee = (EditText) findViewById(R.id.menee);
+
+        ArrayAdapter<CharSequence> adapterVarit = ArrayAdapter.createFromResource(this,
+                R.array.varit, android.R.layout.simple_spinner_item);
+        adapterVarit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        this.varit.setAdapter(adapterVarit);
     }
 
     public void valmis(View view) {
+        String meno = this.menee.getText().toString();
+        String vari = this.varit.getSelectedItem().toString();
         finish();
     }
 }
