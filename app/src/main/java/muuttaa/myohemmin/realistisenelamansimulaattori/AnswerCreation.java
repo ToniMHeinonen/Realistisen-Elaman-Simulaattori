f586045f1889544ede666c78484a491f34523911
Jesse-Stenroth <jesse.stenroth@gmail.com>
2020-03-24 13:08:59 +0200

    add: popup Activity

---
+++ app/src/main/java/muuttaa/myohemmin/realistisenelamansimulaattori/AnswerCreation.java
@@ -0,0 +1,14 @@
+package muuttaa.myohemmin.realistisenelamansimulaattori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnswerCreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer_creation);
+    }
+}
